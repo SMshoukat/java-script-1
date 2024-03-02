aa0077d8ce10258962449a8dfff4cc9d5c65c1fc
SMshoukat <smshoukat11@gmail.com>
2024-03-02 10:24:52 -0800

    ok

---
+++ chapter 9-11.js
@@ -0,0 +1,18 @@
+// q1
+// var city = prompt("Enter your city");
+// if (city == "karachi"){
+//     document.write("<h1>welcome to city of lights</h1>")
+// }
+// else { document.write ("<h1>Andhera hi adhera hai..!</h1>") }
+
+// q2
+var gender = prompt("Enter your gender");
+if(gender == "male"){
+    document.write("<h1>Good Morning Sir.</h1>")
+}
+else if (gender == "female"){
+    document.write("<h1>Good Morning ma'am.</h1>")
+}
+else {
+    document.write("<h1>you are robot.</h1>");
+}
